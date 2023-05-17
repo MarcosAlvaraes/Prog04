21d6ef846b40f7f6c3d41381d76db987bae9eb23
Joao Gomes <joaomfg94@gmail.com>
2023-05-17 16:50:47 +0100

    Ex4

--- Form5.Designer.vb
+++ Form5.Designer.vb
@@ -22,9 +22,91 @@ Partial Class Form5
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Legenda = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Legenda
+        '
+        Me.Legenda.AutoSize = True
+        Me.Legenda.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Legenda.Location = New System.Drawing.Point(193, 44)
+        Me.Legenda.Name = "Legenda"
+        Me.Legenda.Size = New System.Drawing.Size(394, 51)
+        Me.Legenda.TabIndex = 0
+        Me.Legenda.Text = "ISTO E UMA LEGENDA"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(202, 125)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(375, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "a) Esconder a legenda"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(202, 171)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(375, 40)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "b) Mostrar legenda"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(202, 217)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(375, 40)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "c) Mudar cor de fundo da legenda"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(202, 263)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(375, 40)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "b) Mudar cor de fundo do formulário"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(202, 309)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(375, 77)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "e) Escrever na legenda um nº aleatório entre 1 e 100"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 30.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Legenda)
+        Me.Name = "Form5"
         Me.Text = "Form5"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Legenda As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
--- Form5.vb
+++ Form5.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form5
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Legenda.Hide()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Legenda.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Legenda.BackColor = Color.SteelBlue
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.BackColor = Color.Thistle
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Double x = Rnd.Next(0, 100)
+    End Sub
 End Class
